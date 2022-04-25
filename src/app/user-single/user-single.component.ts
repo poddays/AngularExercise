371e6a0dda3a0c47822595275480c1d605abe53a
Giovanni <giovy.pod.da99@outlook.it>
2022-04-25 22:37:10 +0200

    es4Commit

--- src/app/user-single/user-single.component.ts
+++ src/app/user-single/user-single.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { IUser,IAddress,ICompany,roles,genders  } from "../models/IUser-interface";
 @Component({
   selector: 'app-user-single',
   templateUrl: './user-single.component.html',
   styleUrls: ['./user-single.component.css']
 })
 export class UserSingleComponent implements OnInit {
+@Input() user!: IUser; 
 
   constructor() { }
 
